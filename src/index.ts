a181b45e2f9cb577aca081d4212ed49a5851848f
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-05 12:26:25 -0400

    stick request onto context for posterity

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,24 @@ import resolvers from "./resolvers";
 import typeDefs from "./schema";
 
 import * as dotenv from "dotenv";
+import { ContextParameters } from "graphql-yoga/dist/types";
 dotenv.config();
 
 const RIOT_KEY = process.env.RIOT_KEY;
+if (!RIOT_KEY) throw new Error("no RIOT_KEY in .env");
+
+type Context = {
+  RIOT_KEY: string;
+} & ContextParameters;
 
 const server = new GraphQLServer({
   typeDefs,
   // @ts-ignore
   resolvers,
-  context: {
+  context: (request): Context => ({
     RIOT_KEY,
-  },
+    ...request,
+  }),
 });
 
 server.start(({ port }) => {
