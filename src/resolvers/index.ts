5dd9a090d10e6cd5a04c1bfe30502259d3c001b3
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-05 12:27:25 -0400

    resolver for json

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,11 @@
 import { Resolvers } from "../generated/graphql";
 import Query from "./Query";
+import Summoner from "./Summoner";
+import { JSONObjectResolver } from "graphql-scalars";
 
 const resolvers: Resolvers = {
   Query,
+  Summoner,
+  JSON: JSONObjectResolver,
 };
 export default resolvers;
