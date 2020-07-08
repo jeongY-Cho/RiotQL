34897a20f6687f810b6a1bc7f642de409aabdda5
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-08 12:10:03 -0400

    champion rotation resolvers implemented

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -155,72 +155,31 @@ const match: QueryResolvers<Context>["match"] = async (
 //     }
 //   );
 
-//   let count = 0;
-//   Object.values(args).forEach((arg) => {
-//     if (Boolean(arg)) {
-//       count++;
-//     }
-//   });
-
-//   if (count > 1) {
-//     throw new Error(
-//       `only one id type should be supplied got ${count} instead `
-//     );
-//   }
-
-//   let res: AxiosResponse<Omit<Summoner, "region">>;
-//   if (
-//     (selectionSetNames && selectionSetNames.length > 1) ||
-//     !args.encryptedAccountId
-//   ) {
-//     if (args.encryptedAccountId)
-//       res = await SummonerFetchers.byAccountId(region, {
-//         accountId: args.encryptedAccountId,
-//         RIOT_KEY: context.RIOT_KEY,
-//       });
-//     else if (args.encryptedPUUID)
-//       res = await SummonerFetchers.byPUUID(region, {
-//         puuid: args.encryptedPUUID,
-//         RIOT_KEY: context.RIOT_KEY,
-//       });
-//     else if (args.summonerName)
-//       res = await SummonerFetchers.bySummonerName(region, {
-//         summonerName: args.summonerName,
-//         RIOT_KEY: context.RIOT_KEY,
-//       });
-//     else if (args.encryptedSummonerId)
-//       res = await SummonerFetchers.bySummonerId(region, {
-//         summonerId: args.encryptedSummonerId,
-//         RIOT_KEY: context.RIOT_KEY,
-//       });
-//     else {
-//       throw new Error("no id given");
-//     }
-//   } else {
-//     res = {
-//       data: {
-//         accountId: args.encryptedAccountId,
-//       },
-//     } as AxiosResponse;
-//   }
-//   return {
-//     ...res.data,
-//     region,
-//   };
-// };
-
-// const match: QueryResolvers["match"] = async (parent, args, context, info) => {
-//   return (
-//     await matchByMatchId(args.region, {
-//       matchId: args.matchId,
-//       RIOT_KEY: context.RIOT_KEY,
-//     })
-//   ).data;
-// };
-
+const championRotation: QueryResolvers<Context>["championRotation"] = async (
+  parent,
+  args,
+  context,
+  info
+) => {
+  let res = await context.api(
+    "na1",
+    "champion-v3.getChampionInfo",
+    undefined,
+    undefined,
+    {
+      // @ts-expect-error
+      config: {
+        maxAge: 6 * 60 * 1000,
+      },
+    }
+  );
+  if (!res) throw new Error("champion info fetch error");
+  return res.data;
+};
 const QueryResolvers: QueryResolvers = {
   info,
   summoner,
   match,
+  championRotation,
 };
 export default QueryResolvers;
