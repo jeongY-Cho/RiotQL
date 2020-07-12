6b70cef3ce9efeb5b4e16d1f3ff4ff806d90b354
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-12 16:27:43 -0400

    more mirgations

--- src/schema/query/rankedLeague.ts
+++ src/schema/query/rankedLeague.ts
@@ -1,1 +1,67 @@
-// TODO rankedLeague.ts
+import { schema } from "nexus";
+
+schema.extendType({
+  type: "Query",
+  definition(t) {
+    t.field("rankedListabc", {
+      type: "Leaguev4LeagueEntry",
+      list: true,
+      args: {
+        region: schema.arg({
+          type: "RegionInput",
+          required: true,
+        }),
+        queue: schema.arg({
+          type: "AllRankedQueues",
+          required: true,
+        }),
+        tier: schema.arg({
+          type: "LowerTier",
+          required: true,
+        }),
+        division: schema.arg({
+          type: "Division",
+          required: true,
+        }),
+        page: schema.intArg(),
+      },
+      async resolve(root, args, context) {
+        switch (args.queue) {
+          case "RANKED_FLEX_SR":
+          case "RANKED_FLEX_TT":
+          case "RANKED_SOLO_5x5":
+            let leagueRes = await context.api(
+              args.region,
+              "league-v4.getLeagueEntries",
+              {
+                division: args.division,
+                queue: args.queue,
+                tier: args.tier,
+                page: args.page ? args.page : undefined,
+              }
+            );
+            return leagueRes ? leagueRes.data : [];
+          case "RANKED_TFT":
+            let tftRes = await context.api(
+              args.region,
+              "tft-league-v1.getLeagueEntries",
+              {
+                division: args.division,
+                tier: args.tier,
+                page: args.page ? args.page : undefined,
+              }
+            );
+            return tftRes ? tftRes.data : [];
+          default:
+            // TODO valorant ranked queues
+            return [];
+        }
+      },
+    });
+  },
+});
+
+schema.enumType({
+  name: "LowerTier",
+  members: ["IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND"],
+});
