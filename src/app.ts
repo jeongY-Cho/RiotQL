42f79bcef332d430c48e45d52e7dbcd911b8e744
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-13 14:51:35 -0400

    error stuff2

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,71 @@ import OpenAPIClientAxios, {
   AxiosRequestConfig,
 } from "../openapi-client-axios";
 import qs from "qs";
+type OpMethodKeys = keyof OperationMethods;
+export type ApiClient = <T extends OpMethodKeys>(
+  region: Region,
+  endpoint: T,
+  parameters?: Parameters<OperationMethods[T]>[0],
+  data?: Parameters<OperationMethods[T]>[1],
+  config?: Parameters<OperationMethods[T]>[2]
+) => ReturnType<OperationMethods[T]> | Promise<null> | null;
+
+async function apiContext(options?: AxiosRequestConfig) {
+  dotenv.config();
+
+  const RIOT_KEY = process.env.RIOT_KEY;
+  if (!process.env.RIOT_KEY || !process.env.RIOT_OPENAPI_SCHEMA) {
+    throw new Error("no RIOT_KEY or no RIOT_OPENAPI_SCHEMA in .env");
+  }
+
+  const OpenAPI = new OpenAPIClientAxios({
+    definition: "./riot-openapi-schema.json",
+    validate: false,
+    axiosConfigDefaults: {
+      headers: {
+        "X-Riot-Token": process.env.RIOT_KEY,
+      },
+      // @ts-ignore
+      paramsSerializer: function (params) {
+        return qs.stringify(params, { arrayFormat: "repeat" });
+      },
+      ...options,
+    },
+  });
+
+  await OpenAPI.init<Client>();
+  const client = await OpenAPI.getClient<Client>();
+
+  let api = <T extends keyof OperationMethods>(
+    region: Region,
+    endpoint: T,
+    parameters?: Parameters<OperationMethods[T]>[0],
+    data?: Parameters<OperationMethods[T]>[1],
+    config?: Parameters<OperationMethods[T]>[2]
+  ) => {
+    OpenAPI.withServer(0, { platform: region });
+    let baseURL = OpenAPI.getBaseURL();
+
+    let configWithRegion = {
+      baseURL,
+      ...config,
+    };
+
+    try {
+      // @ts-ignore
+      return client[endpoint](parameters, data, configWithRegion) as ReturnType<
+        OperationMethods[T]
+      >;
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status == 404) {
+        return null;
+      }
+      throw err;
+    }
+  };
+  return api as ApiClient;
+}
 try {
   settings.change({
     server: {
@@ -28,75 +93,6 @@ try {
       };
     });
   });
-
-  type OpMethodKeys = keyof OperationMethods;
-
-  export type ApiClient = <T extends OpMethodKeys>(
-    region: Region,
-    endpoint: T,
-    parameters?: Parameters<OperationMethods[T]>[0],
-    data?: Parameters<OperationMethods[T]>[1],
-    config?: Parameters<OperationMethods[T]>[2]
-  ) => ReturnType<OperationMethods[T]> | Promise<null> | null;
-
-  async function apiContext(options?: AxiosRequestConfig) {
-    dotenv.config();
-
-    const RIOT_KEY = process.env.RIOT_KEY;
-    if (!process.env.RIOT_KEY || !process.env.RIOT_OPENAPI_SCHEMA) {
-      throw new Error("no RIOT_KEY or no RIOT_OPENAPI_SCHEMA in .env");
-    }
-
-    const OpenAPI = new OpenAPIClientAxios({
-      definition: "./riot-openapi-schema.json",
-      validate: false,
-      axiosConfigDefaults: {
-        headers: {
-          "X-Riot-Token": process.env.RIOT_KEY,
-        },
-        // @ts-ignore
-        paramsSerializer: function (params) {
-          return qs.stringify(params, { arrayFormat: "repeat" });
-        },
-        ...options,
-      },
-    });
-
-    await OpenAPI.init<Client>();
-    const client = await OpenAPI.getClient<Client>();
-
-    let api = <T extends keyof OperationMethods>(
-      region: Region,
-      endpoint: T,
-      parameters?: Parameters<OperationMethods[T]>[0],
-      data?: Parameters<OperationMethods[T]>[1],
-      config?: Parameters<OperationMethods[T]>[2]
-    ) => {
-      OpenAPI.withServer(0, { platform: region });
-      let baseURL = OpenAPI.getBaseURL();
-
-      let configWithRegion = {
-        baseURL,
-        ...config,
-      };
-
-      try {
-        // @ts-ignore
-        return client[endpoint](
-          parameters,
-          data,
-          configWithRegion
-        ) as ReturnType<OperationMethods[T]>;
-      } catch (err) {
-        console.log(err);
-        if (err.response?.status == 404) {
-          return null;
-        }
-        throw err;
-      }
-    };
-    return api as ApiClient;
-  }
 
   server.express.get("/", (req, res) => {
     res.send("test");
