56e2b6a2d3d3052acc7577071995d332ad647271
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-07-14 18:31:37 -0400

    fix tournament and stub

--- src/schema/query/tournament.ts
+++ src/schema/query/tournament.ts
@@ -1,45 +1,49 @@
-import { schema } from "nexus";
+import { schema } from 'nexus'
+import { APIKeyType } from '../../app'
 
 schema.extendType({
-  type: "Query",
+  type: 'Query',
   definition(t) {
-    t.field("tournament", {
+    t.field('tournament', {
       nullable: true,
       args: {
         code: schema.stringArg({ required: true }),
       },
-      type: "Tournamentv4TournamentCode",
+      type: 'Tournamentv4TournamentCode',
       async resolve(root, args, context) {
         let res = await context.api(
-          "americas",
-          "tournament-v4.getTournamentCode",
+          APIKeyType.League,
+          'americas',
+          'tournament-v4.getTournamentCode',
           {
             tournamentCode: args.code,
-          }
-        );
-        return res ? res.data : null;
+          },
+        )
+        return res ? res.data : null
       },
-    });
+    })
   },
-});
+})
 
 schema.extendType({
-  type: "Tournamentv4TournamentCode",
+  type: 'Tournamentv4TournamentCode',
   definition(t) {
-    t.list.field("lobbyEvents", {
-      type: "Tournamentv4LobbyEvent",
+    t.list.field('lobbyEvents', {
+      type: 'Tournamentv4LobbyEvent',
       async resolve(root, args, context) {
         let res = await context.api(
-          "americas",
-          "tournament-v4.getLobbyEventsByCode",
+          APIKeyType.League,
+
+          'americas',
+          'tournament-v4.getLobbyEventsByCode',
           {
             tournamentCode: root.code,
-          }
-        );
-        return res ? res.data.eventList : [];
+          },
+        )
+        return res ? res.data.eventList : []
       },
-    });
+    })
   },
-});
+})
 
 // TODO tournament.ts mutuations
