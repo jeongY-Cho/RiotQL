2fe8baca7e860ac196e4ef07b15c4fdbd48977bc
Jeong Yeon Cho <jy1998@hotmail.com>
2020-07-15 12:31:45 -0400

    comments

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import testAdapter from './tests/test_adapter'
 
-// set adapter to
+// set adapter to testAdapter
+// test adapter redirects all calls to a mock api with the same openapi schema as riot api
 beforeAll(async () => {
   axios.defaults.adapter = await testAdapter()
 })
